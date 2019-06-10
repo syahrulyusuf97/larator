1786f7377ee65c6d42036f2dfcec36bb5d905b25
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-06-10 11:06:31 +0700

    Initial commit

---
+++ Providers/LaratorServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Syahrulyusuf97\Larator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // register our controller
+        // $this->app->make('Devdojo\Calculator\CalculatorController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        //include __DIR__.'/routes/routes.php'; atau
+        //$this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        //load views
+        //$this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
+        //load migrations
+        // /$this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+    }
+}
